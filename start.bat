3814259fb2f682ae376a0727914dfc15f21daf59
nang <vannaksomnang.dev@gmail.com>
2026-07-02 16:06:57 +0700

    a

--- start.bat
+++ start.bat
@@ -5,6 +5,7 @@ start "Vite - Frontend" cmd /k "cd Frontend && npm run dev"
 timeout /t 3 /nobreak
 
 start http://localhost:5173/Login
+start http://localhost:5173/Login
 start http://127.0.0.1:8000/admin/
 
 
