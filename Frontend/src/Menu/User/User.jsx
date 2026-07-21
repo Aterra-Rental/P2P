32114c4acc680bc88b31e4a531388293dcbf311a
nang <vannaksomnang.dev@gmail.com>
2026-07-21 09:15:51 +0700

    f

--- Frontend/src/Menu/User/User.jsx
+++ Frontend/src/Menu/User/User.jsx
@@ -1,7 +1,6 @@
 // import React, { useState, useEffect } from 'react'
 import { useState, useEffect } from 'react'
 import '../Global.css'
-import './User.css'
 import { useNavigate } from 'react-router-dom'
 
 const statusColors = {
