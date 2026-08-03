085d1288c7d8ea28feb933d0fbd6c2ef2d4d6b94
vannaksomnangdev-dev <vannaksomnang.dev@gmail.com>
2026-08-03 13:55:32 +0700

    i fix mine on change other , just the start.bat

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@
 title P2P Secure Transaction Platform Launcher
 
 echo Starting P2P Python Backend...
-start "P2P - Backend" cmd /k "cd /d C:\Users\USER\Documents\GitHub\P2P\Backend && python app.py"
+start "P2P - Backend" cmd /k "cd /d C:\Users\USER\Documents\GitHub\P2P\Backend\myproject\p2p_deal_app && python app.py"
 
 echo Starting P2P Vite Frontend...
 start "P2P - Frontend" cmd /k "cd /d C:\Users\USER\Documents\GitHub\P2P\Frontend && npm run dev"
@@ -16,7 +16,7 @@ timeout /t 4 /nobreak >nul
 
 echo Opening browser tabs in Chrome...
 start chrome http://localhost:5173/
-
+start chrome http://localhost:5173/Login
 
 echo All services launched successfully!
 
